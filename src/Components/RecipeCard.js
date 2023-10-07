b370f0b28c479490ac6581d64f5568a981ca6580
Sarah Goewey <sarah.goewey@yahoo.com>
2023-10-07 17:36:00 -0400

    heart button fill is working correctly

--- src/Components/RecipeCard.js
+++ src/Components/RecipeCard.js
@@ -11,7 +11,7 @@ const RecipeCard = (recipe) => {
   const [details, setDetails] = useState({ summary: "" });
   const [openItems, setOpenItems] = useState([]);
   const [cleanSummary, setCleanSummary] = useState("");
-  const [isFavorite, setIsFavorite] = useState(false);
+  //const [isFavorite, setIsFavorite] = useState(false);
   const { auth, recipes, favorites } = useSelector((state) => state);
   const dispatch = useDispatch();
 
@@ -76,8 +76,6 @@ const RecipeCard = (recipe) => {
   };
 
   const favorite = async (id) => {
-    setIsFavorite(!isFavorite);
-
     if (isFavorited(id)) {
       const favorite = isFavorited(id);
       dispatch(deleteFavorite(favorite.id));
@@ -111,7 +109,7 @@ const RecipeCard = (recipe) => {
             }}
           >
             {" "}
-            {!isFavorite && (
+            {!isFavorited(recipe.id) && (
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
@@ -123,7 +121,7 @@ const RecipeCard = (recipe) => {
                 <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
               </svg>
             )}
-            {!!isFavorite && (
+            {!!isFavorited(recipe.id) && (
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
