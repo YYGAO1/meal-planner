8623ca6ff4b33c8d8df8e8e1154a41841ba464d7
Angel Gao <angelgao9@gmail.com>
2023-10-19 16:08:50 -0400

    added datepicker

--- src/Components/RecipePage.js
+++ src/Components/RecipePage.js
@@ -3,12 +3,21 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { API_KEY } from "../../env";
 import * as DOMPurify from "dompurify";
+import dayjs from "dayjs";
+import { addToMealPlanner } from "../store";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const RecipePage = () => {
   const { id } = useParams();
   const [details, setDetails] = useState([]);
   const [extendedIngredients, setExtendedIngredients] = useState([]);
   const cleanSummary = DOMPurify.sanitize(details.summary);
+
+  const types = ["snack", "breakfast", "lunch", "dinner"];
+  const today = new Date();
+  const [date, setDate] = useState(today);
+  const [type, setType] = useState("");
 
   useEffect(() => {
     getRecipeDetails(id);
@@ -24,6 +33,12 @@ const RecipePage = () => {
       setExtendedIngredients(filterDuplicates(details.extendedIngredients));
     }
   }, [details.extendedIngredients]);
+
+  //add to planner
+  const addToPlanner = ({ id, type, date }) => {
+    const newDate = dayjs(date).format("YYYY-MM-DD");
+    dispatch(addToMealPlanner({ id, type, date: newDate }));
+  };
 
   const filterDuplicates = (ingredients) => {
     const seen = {};
@@ -67,7 +82,13 @@ const RecipePage = () => {
   return (
     <div className="container text-center">
       <h1>{details.title}</h1>
+
       <h2>insert meal planner add form here</h2>
+      <DatePicker
+        showIcon
+        selected={date}
+        onChange={(newDate) => setDate(newDate)}
+      />
       <img src={details.image} />
       <p dangerouslySetInnerHTML={{ __html: cleanSummary }}></p>
       <h2>ingredients</h2>
