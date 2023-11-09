00b5c468f976e27076da2eddd3f0af9595f11434
Sarah Goewey <sarah.goewey@yahoo.com>
2023-11-09 10:44:29 -0500

    some styling on planner

--- src/Components/MealPlanner.js
+++ src/Components/MealPlanner.js
@@ -85,79 +85,96 @@ const MealPlanner = () => {
   });
 
   return (
-    <>
-      <div className="mealPlannerCalendar">
-        <DatePicker
-          showIcon
-          inline
-          dateFormat="MM-dd-yyyy"
-          selected={date}
-          onChange={(toDate) => setDate(toDate)}
-        />
+    <div style={{ width: "100%" }}>
+      <h1 className="text-secondary">Planner</h1>
+      <div
+        className="card bg-danger"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-around",
+          alignItems: "center",
+          width: "65%",
+          margin: "25px auto",
+          padding: "15px",
+        }}
+      >
+        <div
+          className="mealPlannerCalendar card bg-secondary"
+          style={{ padding: "15px" }}
+        >
+          <DatePicker
+            showIcon
+            inline
+            dateFormat="MM-dd-yyyy"
+            selected={date}
+            onChange={(toDate) => setDate(toDate)}
+          />
+        </div>
+        <div className="mealsOfTheDay text-success" style={{ width: "40%" }}>
+          <h3>Breakfast</h3>
+          <ul style={{ listStyle: "none", padding: "0" }}>
+            {breakfast.map((recipe) => (
+              <li key={recipe.id}>
+                <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+                {/* <p>add to grocery list</p>
+              <i class="bi bi-plus-circle"></i> */}
+              </li>
+            ))}
+          </ul>
+          <h3>Lunch</h3>
+          <ul style={{ listStyle: "none", padding: "0" }}>
+            {lunch.map((recipe) => (
+              <li key={recipe.id}>
+                <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+                {/* <p>add to grocery list</p>
+              <i class="bi bi-plus-circle"></i> */}
+              </li>
+            ))}
+          </ul>{" "}
+          <h3>Dinner</h3>
+          <ul style={{ listStyle: "none", padding: "0" }}>
+            {dinner.map((recipe) => (
+              <li key={recipe.id}>
+                <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+                {/* <p>add to grocery list</p>
+              <i class="bi bi-plus-circle"></i> */}
+              </li>
+            ))}
+          </ul>
+          <h3>Snacks</h3>
+          <ul style={{ listStyle: "none", padding: "0" }}>
+            {snacks.map((recipe) => (
+              <li key={recipe.id}>
+                <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+                {/* <p>add to grocery list</p>
+              <i class="bi bi-plus-circle"></i> */}
+              </li>
+            ))}
+          </ul>
+          <h3>Desserts</h3>
+          <ul style={{ listStyle: "none", padding: "0" }}>
+            {dessert.map((recipe) => (
+              <li key={recipe.id}>
+                <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+                {/* <p>add to grocery list</p>
+              <i class="bi bi-plus-circle"></i> */}
+              </li>
+            ))}
+          </ul>
+          <h3>Misc.</h3>
+          <ul style={{ listStyle: "none", padding: "0" }}>
+            {misc.map((recipe) => (
+              <li key={recipe.id}>
+                <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+                {/* <p>add to grocery list</p>
+              <i class="bi bi-plus-circle"></i> */}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className="mealsOfTheDay">
-        <h3>Breakfast</h3>
-        <ul>
-          {breakfast.map((recipe) => (
-            <li key={recipe.id}>
-              <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
-              {/* <p>add to grocery list</p>
-              <i class="bi bi-plus-circle"></i> */}
-            </li>
-          ))}
-        </ul>
-        <h3>Lunch</h3>
-        <ul>
-          {lunch.map((recipe) => (
-            <li key={recipe.id}>
-              <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
-              {/* <p>add to grocery list</p>
-              <i class="bi bi-plus-circle"></i> */}
-            </li>
-          ))}
-        </ul>{" "}
-        <h3>Dinner</h3>
-        <ul>
-          {dinner.map((recipe) => (
-            <li key={recipe.id}>
-              <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
-              {/* <p>add to grocery list</p>
-              <i class="bi bi-plus-circle"></i> */}
-            </li>
-          ))}
-        </ul>
-        <h3>Snacks</h3>
-        <ul>
-          {snacks.map((recipe) => (
-            <li key={recipe.id}>
-              <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
-              {/* <p>add to grocery list</p>
-              <i class="bi bi-plus-circle"></i> */}
-            </li>
-          ))}
-        </ul>
-        <h3>Desserts</h3>
-        <ul>
-          {dessert.map((recipe) => (
-            <li key={recipe.id}>
-              <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
-              {/* <p>add to grocery list</p>
-              <i class="bi bi-plus-circle"></i> */}
-            </li>
-          ))}
-        </ul>
-        <h3>Misc.</h3>
-        <ul>
-          {misc.map((recipe) => (
-            <li key={recipe.id}>
-              <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
-              {/* <p>add to grocery list</p>
-              <i class="bi bi-plus-circle"></i> */}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
+    </div>
   );
 };
 
