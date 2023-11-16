6147f0668b5d9ff8d506c201ddc0e7bdc979b21e
Sarah Goewey <sarah.goewey@yahoo.com>
2023-11-16 11:51:48 -0500

    removed about and avatar from updateuser

--- src/Components/UpdateUser.js
+++ src/Components/UpdateUser.js
@@ -1,42 +1,20 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
 import { updateAuth } from "../store";
 const UpdateUser = () => {
   const { auth } = useSelector((state) => state);
 
   const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const ref = useRef();
-  const carouselWidth = "50%";
   const [account, setAccount] = useState({
     username: "",
     password: "",
-    avatar: "",
-    about: "",
   });
-
-  useEffect(() => {
-    ref.current.addEventListener("change", (ev) => {
-      const file = ev.target.files[0];
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.addEventListener("load", () => [
-        setAccount((currentVal) => ({
-          ...currentVal,
-          avatar: reader.result,
-        })),
-      ]);
-    });
-  }, [ref]);
 
   useEffect(() => {
     if (auth) {
       setAccount({
         username: auth.username,
         password: auth.password,
-        about: auth.about ? auth.about : "",
-        avatar: auth.avatar ? auth.avatar : "",
       });
     }
   }, [auth]);
@@ -48,7 +26,6 @@ const UpdateUser = () => {
   const update = async (ev) => {
     ev.preventDefault();
     await dispatch(updateAuth(account));
-    //navigate(`/users/${auth.id}`);
   };
 
   return (
@@ -91,45 +68,6 @@ const UpdateUser = () => {
               type="password"
             />
           </label>
-        </div>
-        <div className="mb-3">
-          <label className="form-label text-secondary" style={{ width: "55%" }}>
-            About
-            <textarea
-              className="form-control bg-danger text-success"
-              value={account.about || ""}
-              name="about"
-              onChange={onChange}
-            />
-          </label>
-        </div>
-        <div className="mb-3">
-          <label
-            className="form-label text-secondary"
-            style={{
-              width: "55%",
-              display: "inline-block",
-              padding: "12px 18px",
-              cursor: "pointer",
-              borderRadius: "5px",
-              backgroundColor: "#89bf8e",
-              fontSize: "16px",
-              fontWeight: "bold",
-              color: "#ffe6d4",
-            }}
-          >
-            Upload Avatar (PNG, JPEG, JPG only)
-            <div className="custom-file">
-              <input
-                className="form-control custom-file-input bg-danger text-success"
-                type="file"
-                ref={ref}
-                id="inputGroupFile01"
-                style={{ display: "none" }}
-              />
-            </div>
-          </label>
-          <img src={account.avatar} />
         </div>
         <button className="btn btn-secondary text-success" onClick={update}>
           {" "}
