3b2f6f8000ede9c149901d721858a7c5229c9b0d
Sarah Goewey <sarah.goewey@yahoo.com>
2024-01-10 15:49:43 -0500

    quantity suggestions in quantityform

--- src/Components/QuantityForm.js
+++ src/Components/QuantityForm.js
@@ -5,6 +5,13 @@ import { updateListItem } from "../store";
 const QuantityForm = (item) => {
   const dispatch = useDispatch();
   const [workingItem, setWorkingItem] = useState({ ...item });
+  const { allIngredients } = useSelector((state) => ({
+    allIngredients: state.allIngredients,
+  }));
+
+  const _ingredient = allIngredients.find(
+    (ingredient) => ingredient.id === item.ingredientId
+  );
 
   const onChange = (ev) => {
     setWorkingItem({ ...workingItem, [ev.target.name]: ev.target.value });
@@ -22,6 +29,14 @@ const QuantityForm = (item) => {
         name="quantity"
         onChange={onChange}
       ></input>
+      {!!_ingredient && (
+        <p
+          className="text-success"
+          style={{ margin: "auto", fontStyle: "italic", fontSize: "0.85rem" }}
+        >
+          from recipe: {_ingredient.amount} {_ingredient.measurementUnit}
+        </p>
+      )}
       <button
         className="btn btn-secondary text-success"
         style={{ margin: "5px" }}
